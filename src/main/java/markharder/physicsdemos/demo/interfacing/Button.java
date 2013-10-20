71a49ad139b7afd0c43abcf4ebf8b84419c692e1
Mark Harder <mark.harder899@gmail.com>
2013-10-19 21:15:59 -0400

    add hover effect to Buttons

--- src/main/java/markharder/physicsdemos/demo/interfacing/Button.java
+++ src/main/java/markharder/physicsdemos/demo/interfacing/Button.java
@@ -3,6 +3,9 @@ package markharder.physicsdemos.demo.interfacing;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import markharder.physicsdemos.demo.input.Mouse;
 
 /**
  * A simple java button
@@ -10,21 +13,33 @@ import java.awt.Graphics;
  * @author Mark Harder
  * @version 1.0
  */
-public class Button {
+public class Button extends Rectangle {
     private String name;
     private int index;
+    private boolean hover;
 
     public Button(String name, int index) {
+        super(50, 50 + 60 * index, 300, 50);
         this.name = name;
         this.index = index;
+        this.hover = false;
     }
 
     public void tick() {
+        hover = false;
+
+        if (contains(Mouse.location)) {
+            hover = true;
+        }
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(50, 50 + 60 * index, 300, 50);
+        if (hover) {
+            g.setColor(new Color(0, 0, 255, 200));
+        } else {
+            g.setColor(Color.BLUE);
+        }
+        g.fillRect(x, y, width, height);
 
         g.setColor(Color.WHITE);
         g.setFont(new Font("Times New Roman", Font.PLAIN, 30));
